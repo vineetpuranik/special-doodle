f9eab8634ee33ad487aa9f2a566f7491826de904
rust <vnt.manu@gmail.com>
2023-12-18 15:01:31 -0800

    adding pages and routes

---
+++ doodle-blog/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+      <h1>Doodle Blog</h1>
+      <div id="page-body">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles" element={<ArticlesListPage />} />
+          <Route path="/articles/:articleId" element={<ArticlePage />} />
+        </Routes>
+        
+      </div>
+    </div>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
---
+++ doodle-blog/src/pages/ArticlePage.js
@@ -0,0 +1,7 @@
+const ArticlePage = () => {
+    return (
+        <h1>This is the articles page!</h1>
+    );
+}
+
+export default ArticlePage;
